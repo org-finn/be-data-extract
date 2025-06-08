8ba1c822f668877e71353ad38fbfb28ee45e7702
anacnoda77 <sm1108shin@naver.com>
2025-06-08 23:10:09 +0900

    Remove: 불필요한 파일 제거

--- src/finn_python_server/__init__.py
+++
@@ -1,9 +0,0 @@
-from news import news_data
-from stock import stock_data
-
-def main():
-    #stock_data.get_stock_datas()
-    #news_data.get_news_data()
-    
-if __name__ == "__main__":
-    main()
